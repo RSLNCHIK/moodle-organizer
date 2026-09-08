9eb8fe41b23306183f2393d184f64cc83e3bb9f0
RSLNCHIK <151378705+RSLNCHIK@users.noreply.github.com>
2026-09-08 17:29:24 +0200

    Füge AssignmentList-Komponente hinzu: Implementiere das Laden und Anzeigen von Aufgaben

---
+++ frontend/src/components/AssignmentList.jsx
@@ -0,0 +1,22 @@
+function AssignmentList({ assignments, onAssignmentClick }) {
+    return (
+        <div className="mt-8">
+            <h2 className="mb-4 text-xl font-semidbold">
+                Assignments:
+            </h2>
+
+            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
+                {assignments.map((assignment) => (
+                    <div key={assignment.id}
+                    onClick={() => onAssignmentClick(assignment.id)}
+                    className="cursor-pointer rounded-xl bg-white p-5 hover:shadow-md">
+                        {assignment.name}
+                    </div>
+                ))}
+            </div>
+
+        </div>
+    )
+}
+
+export default AssignmentList;
