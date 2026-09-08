e1dac0abdc4c34331a4914eb865b3add4ba863e2
RSLNCHIK <151378705+RSLNCHIK@users.noreply.github.com>
2026-09-08 17:27:30 +0200

    Füge AssignmentList- und FileList-Komponenten hinzu: Implementiere das Laden und Anzeigen von Aufgaben und Dateien

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import CourseList from "./components/CourseList";
 import Navbar from "./components/Navbar";
 import LoginForm from "./components/LoginForm";
+import AssignmentList from "./components/AssignmentList";
+import FileList from "./components/FileList";
 
 function App() {
   const [email, setEmail] = useState("");
@@ -13,6 +15,13 @@ function App() {
 
   const [courses, setCourses] = useState([]);
   const [error, setError] = useState("");
+
+  const [selectedCourse, setSelectedCourse] = useState(null);
+  const [assignments, setAssignments] = useState([]);
+
+  const [selectedAssignment, setSelectedAssignment] = useState(null);
+  const [files, setFiles] = useState([]);
+  
 
   async function handleLogin(event) {
     event.preventDefault();
@@ -74,6 +83,55 @@ function App() {
     setCourses([]);
   }
 
+  // ${token} comes from the state variable token, which is set when the user logs in. It is used to authenticate the request to the backend API.
+  // loadAssignments takes a ${token} from the localStorage
+  async function loadAssignments(courseId) {
+    const response = await fetch(`http://127.0.0.1:8000/courses/${courseId}/assignments`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    });
+
+    if(!response.ok) {
+      console.log("Fehler beim Laden der Aufgaben");
+      return;
+
+    }
+
+    const data = await response.json();
+
+    setAssignments(data);
+    setSelectedCourse(courseId);
+
+  }
+
+  // await is used to wait for the response from the backend API before continuing with the execution of the code. 
+  // This is important because we need to wait for the data to be loaded before we can set the state variables files and selectedAssignment.
+  async function loadFiles(assignmentId) {
+    const response = await fetch(`http://127.0.0.1:8000/assignments/${assignmentId}/files`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`
+
+      }
+    });
+
+    if (!response.ok) {
+      console.log("Fehler beim Laden der Datein");
+      return;
+
+    }
+
+    const data = await response.json();
+
+    // setFiles is used to set the state variable files with the data received from the backend API. This is used to display the files in the frontend.
+    // setSelectedAssignment is used to set the state variable selectedAssignment with the assignmentId received from the backend API. This is used to display the files of the selected assignment in the frontend.
+    setFiles(data);
+    setSelectedAssignment(assignmentId);
+
+  }
+
   // UseEffect is a React hook that allows to perform side effects in function components. In this case, it is used to load the courses when the token changes.
   useEffect(() => {
     if (token) {
@@ -94,11 +152,22 @@ function App() {
         />
       ) : (
         // p-8 is used to add padding of 2 rem (32px) to all sides of the cotainer.
+        // mb-3 is used to add a margin-bottom of 0.75 rem (12px) to the assignment card.
+        // gap-4 is used to add a gap of 1 rem (16px) between the assignment cards.
+        // space-y-2 is used to add a vertical space of 0.5rem (8px) between the assignment cards.
         <div className="mx-auto max-w-5xl p-8">
 
           <Navbar handleLogout={handleLogout} />
 
-          <CourseList courses={courses} />
+          <CourseList courses={courses} onCourseClick={loadAssignments}/>
+          {selectedCourse && (
+          // mt-8 is used to add a margin-top of 2 rem (32px) to the container. This is used to create space between the list of courses and the list of assignments.
+            <AssignmentList assignments={assignments} onAssignmentClick={loadFiles} />
+          )}
+
+          {selectedAssignment && (
+            <FileList files={files} />
+          )}
 
         </div>
       )}
